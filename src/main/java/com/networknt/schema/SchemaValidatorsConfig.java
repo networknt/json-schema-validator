e97d280152365c8c05f4774cee63a0df082b95cc
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-04-01 08:56:15 -1000

    feat: Add URL mappings (#125)
    * feat: Add URL mappings
    
    URL mappings allows public, normally internet accessible URLs (such as might be externally referenced in a schema) to be mapped to local URLs that might be on the filesystem, at a different external URL, or embedded within a JAR file.
    
    This allows the JsonSchema validator to validate a schema with an external reference to validate against the local copy of that reference when the external reference is not available. Note that when using a mapping, the local copy is always used, and the external reference is not queried.
    
    * Add URL mappings to SchemaValidatorsConfig
    
    - Add URL mappings to `SchemaValidatorsConfig`
    - Remove methods to add mappings in `JsonSchemaFactory`
    - Remove methods to add mappings from URL or JSON in `JsonSchemaFactory.Builder`
    - Use a JSON mappings file and its schema in tests (both as a JSON object to be validated and to load schema locations that needs to be tested for redirection)

--- src/main/java/com/networknt/schema/SchemaValidatorsConfig.java
+++ src/main/java/com/networknt/schema/SchemaValidatorsConfig.java
@@ -1,5 +1,9 @@
 package com.networknt.schema;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.net.URL;
+
 public class SchemaValidatorsConfig {
     /**
      * when validate type, if TYPE_LOOSE = true, will try to convert string to different types to match the type defined in schema.
@@ -18,12 +22,27 @@ public class SchemaValidatorsConfig {
      */
     private boolean elementValidationError = false;
     
+    /**
+     * Map of public, normally internet accessible schema URLs to alternate locations; this allows for offline
+     * validation of schemas that refer to public URLs. This is merged with any mappings the {@link JsonSchemaFactory} 
+     * may have been built with.
+     */
+    private Map<URL, URL> urlMappings = new HashMap<URL, URL>();
+
     public boolean isTypeLoose() {
         return typeLoose;
     }
 
     public void setTypeLoose(boolean typeLoose) {
         this.typeLoose = typeLoose;
+    }
+
+    public Map<URL, URL> getUrlMappings() {
+        return new HashMap<URL, URL>(urlMappings);
+    }
+
+    public void setUrlMappings(Map<URL, URL> urlMappings) {
+        this.urlMappings = urlMappings;
     }
 
     public boolean isMissingNodeAsError() {
@@ -48,5 +67,6 @@ public class SchemaValidatorsConfig {
 
     private void loadDefaultConfig() {
         this.typeLoose = true;
+        this.urlMappings = new HashMap<URL, URL>();
     }
 }
