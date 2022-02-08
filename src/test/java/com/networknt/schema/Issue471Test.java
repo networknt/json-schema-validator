62f418fc91b32686d9f383cf6fff4732afab2069
Michael Ruster <michael.ruster@flowfact.de>
2022-02-08 18:22:45 +0100

    Add German validation messages (#511)
    * Fix grammatic error in validation message text
    
    * Add German translation for validation messages

--- src/test/java/com/networknt/schema/Issue471Test.java
+++ src/test/java/com/networknt/schema/Issue471Test.java
@@ -11,8 +11,54 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Issue471Test {
+    private final String DATA_PATH = "/data/issue471.json";
+    private final String SCHEMA_PATH = "/schema/issue471-2019-09.json";
+
+    // Only one test method is allowed at a time as the ResourceBundle is statically initialized
+
+    @Test
+    @Disabled
+    public void shouldFailV201909_with_enUS() throws Exception {
+        Locale.setDefault(Locale.US);
+        Map<String, String> errorsMap = validate();
+        Assertions.assertEquals("$.title: may only be 10 characters long", errorsMap.get("$.title"));
+        Assertions.assertEquals("$.pictures: there must be a maximum of 2 items in the array", errorsMap.get("$.pictures"));
+    }
+
+    @Test
+    @Disabled
+    public void shouldFailV201909_with_zhCN() throws Exception {
+        Locale.setDefault(Locale.CHINA);
+        Map<String, String> errorsMap = validate();
+        Assertions.assertEquals("$.title：可能只有 10 个字符长", errorsMap.get("$.title"));
+        Assertions.assertEquals("$.pictures：数组中最多必须有 2 个项目", errorsMap.get("$.pictures"));
+    }
+
+    @Test
+    @Disabled
+    public void shouldFailV201909_with_deDE() throws Exception {
+        Locale.setDefault(Locale.GERMANY);
+        Map<String, String> errorsMap = validate();
+        Assertions.assertEquals("$.title darf höchstens 10 Zeichen lang sein", errorsMap.get("$.title"));
+        Assertions.assertEquals("$.pictures: Es dürfen höchstens 2 Elemente in diesem Array sein", errorsMap.get("$.pictures"));
+    }
+
+    private Map<String, String> validate() throws Exception {
+        InputStream schemaInputStream = Issue471Test.class.getResourceAsStream(SCHEMA_PATH);
+        JsonSchema schema = getJsonSchemaFromStreamContentV201909(schemaInputStream);
+        InputStream dataInputStream = Issue471Test.class.getResourceAsStream(DATA_PATH);
+        JsonNode node = getJsonNodeFromStreamContent(dataInputStream);
+
+        Set<ValidationMessage> validationMessages = schema.validate(node);
+        return convertValidationMessagesToMap(validationMessages);
+    }
+
+    private Map<String, String> convertValidationMessagesToMap(Set<ValidationMessage> validationMessages) {
+        return validationMessages.stream().collect(Collectors.toMap(ValidationMessage::getPath, ValidationMessage::getMessage));
+    }
 
     private JsonSchema getJsonSchemaFromStreamContentV201909(InputStream schemaContent) {
         JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
@@ -24,47 +70,4 @@ public class Issue471Test {
         return mapper.readTree(content);
     }
 
-    // Only one test method is allowed at a time
-
-    @Test
-    @Disabled
-    public void shouldFailV201909_with_enUS() throws Exception {
-        Locale.setDefault(Locale.US);
-        Set<ValidationMessage> errors = validate();
-        Map<String, String> errorMsgMap = transferErrorMsg(errors);
-        Assertions.assertTrue(errorMsgMap.containsKey("$.title"), "error message must contains key: $.title");
-        Assertions.assertTrue(errorMsgMap.containsKey("$.pictures"), "error message must contains key: $.pictures");
-        Assertions.assertEquals("$.title: may only be 10 characters long", errorMsgMap.get("$.title"));
-        Assertions.assertEquals("$.pictures: there must be a maximum of 2 items in the array", errorMsgMap.get("$.pictures"));
-    }
-
-    @Test
-    @Disabled
-    public void shouldFailV201909_with_zhCN() throws Exception {
-        Locale.setDefault(Locale.CHINA);
-        Set<ValidationMessage> errors = validate();
-        Map<String, String> errorMsgMap = transferErrorMsg(errors);
-        Assertions.assertTrue(errorMsgMap.containsKey("$.title"), "error message must contains key: $.title");
-        Assertions.assertTrue(errorMsgMap.containsKey("$.pictures"), "error message must contains key: $.pictures");
-        Assertions.assertEquals("$.title：可能只有 10 个字符长", errorMsgMap.get("$.title"));
-        Assertions.assertEquals("$.pictures：数组中最多必须有 2 个项目", errorMsgMap.get("$.pictures"));
-    }
-
-    private Set<ValidationMessage> validate() throws Exception {
-        String schemaPath = "/schema/issue471-2019-09.json";
-        String dataPath = "/data/issue471.json";
-        InputStream schemaInputStream = Issue471Test.class.getResourceAsStream(schemaPath);
-        JsonSchema schema = getJsonSchemaFromStreamContentV201909(schemaInputStream);
-        InputStream dataInputStream = Issue471Test.class.getResourceAsStream(dataPath);
-        JsonNode node = getJsonNodeFromStreamContent(dataInputStream);
-        return schema.validate(node);
-    }
-
-    private Map<String, String> transferErrorMsg(Set<ValidationMessage> validationMessages) {
-        Map<String, String> pathToMessage = new HashMap<>();
-        validationMessages.forEach(msg -> {
-            pathToMessage.put(msg.getPath(), msg.getMessage());
-        });
-        return pathToMessage;
-    }
 }
