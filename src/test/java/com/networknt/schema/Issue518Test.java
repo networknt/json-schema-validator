c65900c4e968206c4abf343ee64067d293100218
Piotr Poniedziałek <pondzix@gmail.com>
2022-02-23 21:38:47 +0100

    Add test reproducing issue #518

---
+++ src/test/java/com/networknt/schema/Issue518Test.java
@@ -0,0 +1,29 @@
+package com.networknt.schema;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+public class Issue518Test {
+    private static final JsonMetaSchema igluMetaSchema =
+            JsonMetaSchema
+                    .builder("http://iglucentral.com/schemas/com.snowplowanalytics.self-desc/schema/jsonschema/1-0-0#", JsonMetaSchema.getV7())
+                    .build();
+
+    private static final JsonSchemaFactory FACTORY =
+            JsonSchemaFactory
+                    .builder(JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7))
+                    .addMetaSchema(igluMetaSchema)
+                    .forceHttps(false)
+                    .build();
+
+    @Test
+    public void testPreservingEmptyFragmentSuffix() {
+        String schemaPath = "/schema/issue518-v7.json";
+        InputStream schemaInputStream = getClass().getResourceAsStream(schemaPath);
+        JsonSchema schema = FACTORY.getSchema(schemaInputStream);
+
+        Assertions.assertNotNull(schema);
+    }
+}
