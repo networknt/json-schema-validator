678ff47bdb387688944c2fe32f94fe2fcf79b21a
Christoph Dreis <dreis2211@users.noreply.github.com>
2022-04-30 03:25:05 +0200

    Remove unused variable in JsonNodeUtil (#566)

--- src/main/java/com/networknt/schema/utils/JsonNodeUtil.java
+++ src/main/java/com/networknt/schema/utils/JsonNodeUtil.java
@@ -83,7 +83,6 @@ public class JsonNodeUtil {
                 return true;
             }
 
-            ValidatorState state = (ValidatorState) CollectorContext.getInstance().get(ValidatorState.VALIDATOR_STATE_KEY);
             if(JsonType.NULL.equals(nodeType)) {
                 if(parentSchema != null) {
                     if( parentSchema.getParentSchema() != null && JsonNodeUtil.isNodeNullable(parentSchema.getParentSchema().getSchemaNode(), config) || JsonNodeUtil.isNodeNullable(parentSchema.getSchemaNode()) ) {
