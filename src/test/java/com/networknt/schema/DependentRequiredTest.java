b888a00759acb2b36dc41d8864c72077e0299f89
bernie-schelberg-mywave <113865139+bernie-schelberg-mywave@users.noreply.github.com>
2023-02-15 23:34:07 +1000

    [#475] Make DependentRequired error message more helpful (#661)

---
+++ src/test/java/com/networknt/schema/DependentRequiredTest.java
@@ -0,0 +1,66 @@
+package com.networknt.schema;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+
+class DependentRequiredTest {
+
+    public static final String SCHEMA =
+        "{ " +
+            "   \"$schema\":\"http://json-schema.org/draft/2019-09/schema\"," +
+            "   \"type\": \"object\"," +
+            "   \"properties\": {" +
+            "       \"optional\": \"string\"," +
+            "       \"requiredWhenOptionalPresent\": \"string\"" +
+            "   }," +
+            "   \"dependentRequired\": {" +
+            "       \"optional\": [ \"requiredWhenOptionalPresent\" ]," +
+            "       \"otherOptional\": [ \"otherDependentRequired\" ]" +
+            "   }" +
+            "}";
+
+    private static final JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
+    private static final JsonSchema schema = factory.getSchema(SCHEMA);
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    void shouldReturnNoErrorMessagesForObjectWithoutOptionalField() throws IOException {
+
+        Set<ValidationMessage> messages = whenValidate("{}");
+
+        assertThat(messages, empty());
+    }
+
+    @Test
+    void shouldReturnErrorMessageForObjectWithoutDependentRequiredField() throws IOException {
+
+        Set<ValidationMessage> messages = whenValidate("{ \"optional\": \"present\" }");
+
+        assertThat(
+            messages.stream().map(ValidationMessage::getMessage).collect(Collectors.toList()),
+            contains("$: has a missing property \"requiredWhenOptionalPresent\" which is dependent required because \"optional\" is present"));
+    }
+
+    @Test
+    void shouldReturnNoErrorMessagesForObjectWithOptionalAndDependentRequiredFieldSet() throws JsonProcessingException {
+
+        Set<ValidationMessage> messages =
+            whenValidate("{ \"optional\": \"present\", \"requiredWhenOptionalPresent\": \"present\" }");
+
+        assertThat(messages, empty());
+    }
+
+    private static Set<ValidationMessage> whenValidate(String content) throws JsonProcessingException {
+        return schema.validate(mapper.readTree(content));
+    }
+
+}
