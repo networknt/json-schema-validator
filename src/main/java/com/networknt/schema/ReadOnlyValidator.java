cb248c3e32b4f0fa69836246b8f28d62b8356362
Jorge Sartori <jorge.rocha@serpro.gov.br>
2023-03-24 15:11:00 -0300

    changing ReadOnlyValidator to use boolean property instead of array. Including the concept of read/write modes.

--- src/main/java/com/networknt/schema/ReadOnlyValidator.java
+++ src/main/java/com/networknt/schema/ReadOnlyValidator.java
@@ -16,82 +16,34 @@
 
 package com.networknt.schema;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class ReadOnlyValidator extends BaseJsonValidator implements JsonValidator {
     private static final Logger logger = LoggerFactory.getLogger(RequiredValidator.class);
 
-    private List<String> fieldNames = new ArrayList<String>();
+    private Boolean writeMode;
 
-    public ReadOnlyValidator(String schemaPath, JsonNode schemaNode, JsonSchema parentSchema, ValidationContext validationContext) {
+    public ReadOnlyValidator(String schemaPath, JsonNode schemaNode, JsonSchema parentSchema,
+            ValidationContext validationContext) {
         super(schemaPath, schemaNode, parentSchema, ValidatorTypeCode.READ_ONLY, validationContext);
-        if (schemaNode.isArray()) {
-            int size = schemaNode.size();
-            for (int i = 0; i < size; i++) {
-                fieldNames.add(schemaNode.get(i).asText());
-            }
-        }
-
+        this.writeMode = validationContext.getConfig().isWriteMode();
+        String mode = writeMode ? "write mode" : "read mode";
+        logger.debug("Loaded ReadOnlyValidator for property {} as {}", parentSchema, mode);
         parseErrorCode(getValidatorType().getErrorCodeKey());
     }
 
     public Set<ValidationMessage> validate(JsonNode node, JsonNode rootNode, String at) {
         debug(logger, node, rootNode, at);
-
-        Set<ValidationMessage> errors = new LinkedHashSet<ValidationMessage>();
-
-        for (String fieldName : fieldNames) {
-            JsonNode propertyNode = node.get(fieldName);
-            String datapath = "";
-            if (at.equals("$")) {
-                datapath = datapath + "#original." + fieldName;
-            } else {
-                datapath = datapath + "#original." + at.substring(2) + "." + fieldName;
-            }
-            JsonNode originalNode = getNode(datapath, rootNode);
-
-            boolean theSame = propertyNode != null && originalNode != null && propertyNode.equals(originalNode);
-            if (!theSame) {
-                errors.add(buildValidationMessage(at));
-            }
+        if (writeMode) {
+            return Set.of(buildValidationMessage(at));
+        } else {
+            return Set.of();
         }
-
-        return Collections.unmodifiableSet(errors);
-    }
-
-    private JsonNode getNode(String datapath, JsonNode data) {
-        String path = getSubString(datapath,"$.");
-
-        String[] parts = path.split("\\.");
-        JsonNode result = null;
-        for (int i = 0; i < parts.length; i++) {
-            if (parts[i].contains("[")) {
-                int idx1 = parts[i].indexOf("[");
-                int idx2 = parts[i].indexOf("]");
-                String key = parts[i].substring(0, idx1).trim();
-                int idx = Integer.parseInt(parts[i].substring(idx1 + 1, idx2).trim());
-                result = data.get(key).get(idx);
-            } else {
-                result = data.get(parts[i]);
-            }
-            if (result == null) {
-                break;
-            }
-            data = result;
-        }
-        return result;
-    }
-
-    private String getSubString(String datapath, String keyword){
-        String path = datapath;
-        if (path.startsWith(keyword)) {
-            path = path.substring(2);
-        }
-        return path;
     }
 
 }
