e672edbe6229fa756d7aeaad4afdc3ebd0708018
Steve Hu <stevehu@gmail.com>
2023-03-27 10:18:21 -0400

    Issue683 (#684)
    * changing ReadOnlyValidator to use boolean property instead of array.
    Including the concept of read/write modes.
    
    * updating to keep java 8 compatibility
    
    * fixes #683 update to resolve javadoc warnnings
    
    ---------
    
    Co-authored-by: Jorge Sartori <jorge.rocha@serpro.gov.br>

---
+++ src/test/java/com/networknt/schema/ReadOnlyValidatorTest.java
@@ -0,0 +1,61 @@
+package com.networknt.schema;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+class ReadOnlyValidatorTest {
+
+	@Test
+	void givenConfigWriteFalseWhenReadOnlyTrueThenAllows() throws IOException {
+		ObjectNode node = getJsonNode();
+		Set<ValidationMessage> errors = loadJsonSchema(false).validate(node);
+		assertTrue(errors.isEmpty());
+	}
+
+	@Test
+	void givenConfigWriteTrueWhenReadOnlyTrueThenDenies() throws IOException {
+		ObjectNode node = getJsonNode();
+		Set<ValidationMessage> errors = loadJsonSchema(true).validate(node);
+		assertFalse(errors.isEmpty());
+		assertEquals("$.firstName: is a readonly field, it cannot be changed",
+				errors.stream().map(e -> e.getMessage()).collect(Collectors.toList()).get(0));
+	}
+
+	private JsonSchema loadJsonSchema(Boolean write) {
+		JsonSchema schema = this.getJsonSchema(write);
+		schema.initializeValidators();
+		return schema;
+
+	}
+
+	private JsonSchema getJsonSchema(Boolean write) {
+		JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012);
+		SchemaValidatorsConfig schemaConfig = createSchemaConfig(write);
+		InputStream schema = getClass().getClassLoader().getResourceAsStream("schema/read-only-schema.json");
+		return factory.getSchema(schema, schemaConfig);
+	}
+
+	private SchemaValidatorsConfig createSchemaConfig(Boolean write) {
+		SchemaValidatorsConfig config = new SchemaValidatorsConfig();
+		config.setWriteMode(write);
+		return config;
+	}
+
+	private ObjectNode getJsonNode() throws IOException {
+		InputStream node = getClass().getClassLoader().getResourceAsStream("data/read-only-data.json");
+		ObjectMapper mapper = new ObjectMapper();
+		return (ObjectNode) mapper.readTree(node);
+	}
+
+}
