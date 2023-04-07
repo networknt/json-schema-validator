132a616ac67cd044e8dccd5ac2ae157b6229bd3e
fdutton <fgdutton@gmail.com>
2023-04-07 17:11:50 -0400

    Uses JUnit dynamic tests to generate tests from specification files (#691)
    Co-authored-by: fdutton <fdutton@noreply>

--- src/test/java/com/networknt/schema/BaseSuiteJsonSchemaTest.java
+++ src/test/java/com/networknt/schema/BaseSuiteJsonSchemaTest.java
@@ -16,33 +16,22 @@
 
 package com.networknt.schema;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import io.undertow.Undertow;
 import io.undertow.server.handlers.resource.FileResourceManager;
-import com.networknt.schema.utils.StringUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-
 import java.io.File;
-import java.io.InputStream;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import static io.undertow.Handlers.resource;
-import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class BaseSuiteJsonSchemaTest {
-    protected ObjectMapper mapper = new ObjectMapper();
-    protected JsonSchemaFactory validatorFactory;
+
     protected static Undertow server = null;
 
-    protected BaseSuiteJsonSchemaTest(SpecVersion.VersionFlag version) {
-        validatorFactory = JsonSchemaFactory.builder(JsonSchemaFactory.getInstance(version)).objectMapper(mapper).build();
-    }
+    protected ObjectMapper mapper = new ObjectMapper();
 
     @BeforeAll
     public static void setUp() {
@@ -70,98 +59,10 @@ public abstract class BaseSuiteJsonSchemaTest {
         }
     }
 
-    protected void runTestFile(String testCaseFile) throws Exception {
-        final URI testCaseFileUri = URI.create("classpath:" + testCaseFile);
-        InputStream in = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream(testCaseFile);
-        ArrayNode testCases = mapper.readValue(in, ArrayNode.class);
-        final String VALIDATION_MESSAGES = "validationMessages";
-        for (int j = 0; j < testCases.size(); j++) {
-            try {
-                JsonNode testCase = testCases.get(j);
-                SchemaValidatorsConfig config = new SchemaValidatorsConfig();
-
-                ArrayNode testNodes = (ArrayNode) testCase.get("tests");
-                for (int i = 0; i < testNodes.size(); i++) {
-                    JsonNode test = testNodes.get(i);
-                    JsonNode node = test.get("data");
-                    JsonNode typeLooseNode = test.get("isTypeLoose");
-                    // Configure the schemaValidator to set typeLoose's value based on the test file,
-                    // if test file do not contains typeLoose flag, use default value: true.
-                    config.setTypeLoose((typeLooseNode == null) ? false : typeLooseNode.asBoolean());
-                    JsonSchema schema = validatorFactory.getSchema(testCaseFileUri, testCase.get("schema"), config);
-                    List<ValidationMessage> errors = new ArrayList<ValidationMessage>();
-
-                    errors.addAll(schema.validate(node));
-                    // Clear CollectorContext after every test.
-                    if (test.get("valid").asBoolean()) {
-                        if (!errors.isEmpty()) {
-                            System.out.println("---- test case failed ----");
-                            System.out.println("Description: " + test.get("description"));
-                            System.out.println("schema: " + schema.toString());
-                            System.out.println("data: " + test.get("data"));
-                            System.out.println("errors: " + errors);
-                        }
-                        assertEquals(0, errors.size());
-                    } else {
-                        if (errors.isEmpty()) {
-                            System.out.println("---- test case failed ----");
-                            System.out.println("Description: " + test.get("description"));
-                            System.out.println("schema: " + schema);
-                            System.out.println("data: " + test.get("data"));
-                        } else {
-                            JsonNode errorCount = test.get("errorCount");
-                            if (errorCount != null && errorCount.isInt() && errors.size() != errorCount.asInt()) {
-                                System.out.println("---- test case failed ----");
-                                System.out.println("Description: " + test.get("description"));
-                                System.out.println("schema: " + schema);
-                                System.out.println("data: " + test.get("data"));
-                                System.out.println("errors: " + errors);
-                                assertEquals(errorCount.asInt(), errors.size(), "expected error count");
-                            }
-                        }
-                        assertEquals(false, errors.isEmpty());
-                    }
-
-                    // ExpectedValidation Messages need not be exactly same as actual errors.. the below code checks if expected validation message is subset of actual errors
-                    ArrayNode expectedValidationMesgs = (ArrayNode) test.get(VALIDATION_MESSAGES);
-                    if (errors.isEmpty() && expectedValidationMesgs != null && expectedValidationMesgs.size() > 0) {
-                        System.out.println("---- test case failed ----");
-                        System.out.println("Description: " + test.get("description"));
-                        System.out.println("schema: " + schema);
-                        System.out.println("data: " + test.get("data"));
-                        System.out.println("Expected Validation Messages: " + expectedValidationMesgs);
-                        fail("Expected errors but no errors encountered during validation.");
-
-                    } else if (expectedValidationMesgs != null) {
-                        Iterator<JsonNode> it = expectedValidationMesgs.iterator();
-                        while (it.hasNext()) {
-                            boolean found = false;
-                            String expectedMsg = it.next().textValue();
-                            for (ValidationMessage actualMsg : errors) {
-                                if (StringUtils.equals(expectedMsg, actualMsg.getMessage())) {
-                                    found = true;
-                                    break;
-                                }
-                            }
-                            if (!found) {
-                                System.out.println("---- test case failed ----");
-                                System.out.println("Description: " + test.get("description"));
-                                System.out.println("schema: " + schema);
-                                System.out.println("data: " + test.get("data"));
-                                System.out.println("errors: " + errors);
-                                System.out.println("validationMessages: " + expectedValidationMesgs);
-                                fail("Expected validation message is not found in actual validation messages");
-                                break;
-                            }
-                        }
-                    }
-
-                    CollectorContext.getInstance().reset();
-                }
-            } catch (JsonSchemaException e) {
-                throw new IllegalStateException(String.format("Current schema should not be invalid: %s", testCaseFile), e);
-            }
-        }
+    @AfterEach
+    protected void cleanup() {
+        // Clear CollectorContext after every test.
+        CollectorContext.getInstance().reset();
     }
+
 }
---
+++ src/test/java/com/networknt/schema/TestCase.java
@@ -0,0 +1,135 @@
+package com.networknt.schema;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * An individual test case, containing multiple testSpecs of a single schema's
+ * behavior
+ */
+public class TestCase {
+
+    /**
+     * The test case description (Required)
+     */
+    private final String description;
+
+    /**
+     * Any additional comments about the test case
+     */
+    private final String comment;
+
+    /**
+     * A valid JSON Schema (one written for the corresponding version directory that
+     * the file sits within). (Required)
+     */
+    private final JsonNode schema;
+
+    /**
+     * A set of related tests all using the same schema (Required)
+     */
+    private final List<TestSpec> tests;
+
+    /**
+     * Indicates whether this test-case should be executed
+     * <p>
+     * This is an extension of the schema used to describe tests in the
+     * compliance suite
+     * </p>
+     */
+    private final boolean disabled;
+
+    /**
+     * The location of the specification file containing this test-case.
+     */
+    private Path specification;
+
+    /**
+     * Constructs a new TestCase
+     * 
+     * @param description The test case description (Required)
+     * @param comment Any additional comments about the test case
+     * @param schema A valid JSON Schema (one written for the corresponding version directory that the file sits within) (Required)
+     * @param tests A set of related tests all using the same schema (Required)
+     */
+    @JsonCreator
+    public TestCase(
+        @JsonProperty("description") String description,
+        @JsonProperty("comment") String comment,
+        @JsonProperty("schema") JsonNode schema,
+        @JsonProperty("disabled") Boolean disabled,
+        @JsonProperty("tests") List<TestSpec> tests
+    ) {
+        this.description = description;
+        this.comment = comment;
+        this.schema = schema;
+        this.disabled = Boolean.TRUE.equals(disabled);
+
+        this.tests = tests;
+        if (null != tests) {
+            tests.forEach(test -> test.setTestCase(this));
+        }
+    }
+
+    /**
+     * Identifies the specification file containing this test-case.
+     * @return the path to the specification
+     */
+    public Path getSpecification() {
+        return specification;
+    }
+
+    /**
+     * Sets the location of the specification file containing this test-case.
+     * @param specification the path to the specification
+     */
+    public void setSpecification(Path specification) {
+        this.specification = specification;
+    }
+
+    /**
+     * The test case description (Required)
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDisplayName() {
+        return String.format("%s (%s)", getDescription(), getSpecification());
+    }
+
+    /**
+     * Any additional comments about the test case
+     */
+    public String getComment() {
+        return comment;
+    }
+
+    /**
+     * A valid JSON Schema (one written for the corresponding version directory that
+     * the file sits within). (Required)
+     */
+    public JsonNode getSchema() {
+        return schema;
+    }
+
+    /**
+     * Indicates whether this test-case should be executed
+     */
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    /**
+     * A set of related tests all using the same schema (Required)
+     */
+    public List<TestSpec> getTests() {
+        return null != tests ? tests : Collections.emptyList();
+    }
+
+}
