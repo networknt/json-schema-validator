fa489d94b8f4b9fd3cee19ea6d303944fa215009
Ketul Patel <ketulpatel3012@gmail.com>
2023-11-05 21:00:16 -0400

    Added PrefixItemsValidatorTest for exception handling test case

---
+++ src/test/java/com/networknt/schema/PrefixItemsValidatorTest.java
@@ -0,0 +1,37 @@
+package com.networknt.schema;
+
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * This class handles exception case for {@link PrefixItemsValidator}
+ */
+public class PrefixItemsValidatorTest extends AbstractJsonSchemaTestSuite {
+
+    /**
+     * this method create test cases from JSON and run those test cases with assertion
+     */
+    @Test
+    void testEmptyPrefixItemsException() {
+        Stream<DynamicNode> dynamicNodeStream = createTests(SpecVersion.VersionFlag.V7, "src/test/suite/tests/prefixItemsException");
+        dynamicNodeStream.forEach(
+                dynamicNode -> {
+                    ((DynamicContainer) dynamicNode).getChildren().forEach(dynamicNode1 -> {
+                        if (dynamicNode1 instanceof DynamicTest) {
+                            assertThrows(JsonSchemaException.class, () -> {
+                                ((DynamicTest) dynamicNode1).getExecutable().execute();
+                            });
+                        }
+                    });
+                }
+        );
+    }
+
+
+}
