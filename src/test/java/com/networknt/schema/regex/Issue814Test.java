0924dfe1e466ca4c52bd748b98ea2b53420f348e
Stephan Schroevers <stephan202@gmail.com>
2023-12-02 15:45:04 +0100

    Fix JDK regex support (#888)
    Summary of changes:
    - Fix the test resources introduced by #783 by moving the `regex`
      fields, such that the test framework does not skip them with a "Not a
      valid test case" message.
    - Revert the changes introduced by #815, as those are simply incorrect.
    - Extend the test coverage introduced by #815 by (a) updating the test
      regexes to match their intended semantics and (b) include a few
      negative test cases.
    - Partially revert the change introduced by #783: the use of
      `Matcher#find()` is correct, but the `hasStartAnchor` and
      `hasEndAnchor` logic introduces more bugs than the issue it aims to
      solve.
    - Extend the test coverage introduced by #783, by introducing regexes
      that are not covered by the `hasStartAnchor`/`hasEndAnchor` logic.
    - Update the Joni regular expression integration such that it passes
      more of the test cases.
    - Disable the "trailing newline" test cases, as these are currently not
      handled correctly by either regex implementation.

--- src/test/java/com/networknt/schema/regex/Issue814Test.java
+++ src/test/java/com/networknt/schema/regex/Issue814Test.java
@@ -8,40 +8,62 @@ class Issue814Test {
 
     @Test
     void jdkTypePattern() {
-        JDKRegularExpression ex = new JDKRegularExpression("^list|date|time|string|enum|int|double|long|boolean|number$");
+        JDKRegularExpression ex = new JDKRegularExpression("^(list|date|time|string|enum|int|double|long|boolean|number)$");
         assertTrue(ex.matches("list"));
         assertTrue(ex.matches("string"));
         assertTrue(ex.matches("boolean"));
         assertTrue(ex.matches("number"));
         assertTrue(ex.matches("enum"));
+        assertFalse(ex.matches("listZ"));
+        assertFalse(ex.matches("AenumZ"));
+        assertFalse(ex.matches("Anumber"));
     }
 
     @Test
     void jdkOptionsPattern() {
-        JDKRegularExpression ex = new JDKRegularExpression("^\\d*|[a-zA-Z_]+$");
-        assertTrue(ex.matches("external"));
-        assertTrue(ex.matches("external_gte"));
-        assertTrue(ex.matches("force"));
-        assertTrue(ex.matches("internal"));
+        JDKRegularExpression ex = new JDKRegularExpression("^\\d|[a-zA-Z_]$");
+        assertTrue(ex.matches("5"));
+        assertTrue(ex.matches("55"));
+        assertTrue(ex.matches("5%"));
+        assertTrue(ex.matches("a"));
+        assertTrue(ex.matches("aa"));
+        assertTrue(ex.matches("%a"));
+        assertTrue(ex.matches("%_"));
+        assertTrue(ex.matches("55aa"));
+        assertTrue(ex.matches("5%%a"));
+        assertFalse(ex.matches(""));
+        assertFalse(ex.matches("%"));
+        assertFalse(ex.matches("a5"));
     }
 
     @Test
     void joniTypePattern() {
-        JoniRegularExpression ex = new JoniRegularExpression("^list|date|time|string|enum|int|double|long|boolean|number$");
+        JoniRegularExpression ex = new JoniRegularExpression("^(list|date|time|string|enum|int|double|long|boolean|number)$");
         assertTrue(ex.matches("list"));
         assertTrue(ex.matches("string"));
         assertTrue(ex.matches("boolean"));
         assertTrue(ex.matches("number"));
         assertTrue(ex.matches("enum"));
+        assertFalse(ex.matches("listZ"));
+        assertFalse(ex.matches("AenumZ"));
+        assertFalse(ex.matches("Anumber"));
     }
 
     @Test
     void joniOptionsPattern() {
-        JoniRegularExpression ex = new JoniRegularExpression("^\\d*|[a-zA-Z_]+$");
-        assertTrue(ex.matches("internal"));
-        assertTrue(ex.matches("external"));
-        assertTrue(ex.matches("external_gte"));
-        assertTrue(ex.matches("force"));
+        JoniRegularExpression ex = new JoniRegularExpression("^\\d|[a-zA-Z_]$");
+        assertTrue(ex.matches("5"));
+        assertTrue(ex.matches("55"));
+        assertTrue(ex.matches("5%"));
+        assertTrue(ex.matches("a"));
+        assertTrue(ex.matches("aa"));
+        assertTrue(ex.matches("%a"));
+        assertTrue(ex.matches("%_"));
+        assertTrue(ex.matches("55aa"));
+        assertTrue(ex.matches("5%%a"));
+        assertFalse(ex.matches(""));
+        assertFalse(ex.matches("%"));
+        assertFalse(ex.matches("a5"));
     }
 
 }
