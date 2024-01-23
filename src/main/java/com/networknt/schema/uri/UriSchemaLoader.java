0b6009cd60eff1523f8317d5959ba06fba6d1dd7
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2024-01-23 15:18:04 +0800

    Update uri schema loader

--- src/main/java/com/networknt/schema/uri/UriSchemaLoader.java
+++ src/main/java/com/networknt/schema/uri/UriSchemaLoader.java
@@ -15,7 +15,12 @@
  */
 package com.networknt.schema.uri;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 import com.networknt.schema.AbsoluteIri;
 
@@ -26,6 +31,48 @@ public class UriSchemaLoader implements SchemaLoader {
     @Override
     public InputStreamSource getSchema(AbsoluteIri absoluteIri) {
         URI uri = URI.create(absoluteIri.toString());
-        return () -> uri.toURL().openStream();
+        return () -> {
+            URLConnection conn = uri.toURL().openConnection();
+            return this.openConnectionCheckRedirects(conn);
+        };
+    }
+
+    // https://www.cs.mun.ca/java-api-1.5/guide/deployment/deployment-guide/upgrade-guide/article-17.html
+    protected InputStream openConnectionCheckRedirects(URLConnection c) throws IOException {
+        boolean redir;
+        int redirects = 0;
+        InputStream in = null;
+        do {
+            if (c instanceof HttpURLConnection) {
+                ((HttpURLConnection) c).setInstanceFollowRedirects(false);
+            }
+            // We want to open the input stream before getting headers
+            // because getHeaderField() et al swallow IOExceptions.
+            in = c.getInputStream();
+            redir = false;
+            if (c instanceof HttpURLConnection) {
+                HttpURLConnection http = (HttpURLConnection) c;
+                int stat = http.getResponseCode();
+                if (stat >= 300 && stat <= 307 && stat != 306 && stat != HttpURLConnection.HTTP_NOT_MODIFIED) {
+                    URL base = http.getURL();
+                    String loc = http.getHeaderField("Location");
+                    URL target = null;
+                    if (loc != null) {
+                        target = new URL(base, loc);
+                    }
+                    http.disconnect();
+                    // Redirection should be allowed only for HTTP and HTTPS
+                    // and should be limited to 5 redirections at most.
+                    if (target == null || !(target.getProtocol().equals("http") || target.getProtocol().equals("https"))
+                            || redirects >= 5) {
+                        throw new SecurityException("illegal URL redirect");
+                    }
+                    redir = true;
+                    c = target.openConnection();
+                    redirects++;
+                }
+            }
+        } while (redir);
+        return in;
     }
 }
