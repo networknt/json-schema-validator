bd77903095d84707a5dcf60a7d88ece1750c6278
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2024-02-06 20:25:41 +0800

    Refactor format to allow more complex validation

--- src/main/java/com/networknt/schema/Format.java
+++ src/main/java/com/networknt/schema/Format.java
@@ -16,23 +16,106 @@
 
 package com.networknt.schema;
 
+import java.util.Collections;
+import java.util.Set;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 /**
  * Used to implement the various formats for the format keyword.
+ * <p>
+ * Simple implementations need only override {@link #matches(ExecutionContext, String)}.
  */
 public interface Format {
     /**
+     * Gets the format name.
+     *
      * @return the format name as referred to in a json schema format node.
      */
     String getName();
+    
+    /**
+     * Gets the error message description.
+     *
+     * @return the error message description.
+     */
+    String getErrorMessageDescription();
+
 
     /**
      * Determines if the value matches the format.
-     * 
+     * <p>
+     * This should be implemented for string node types.
+     *
      * @param executionContext the execution context
      * @param value            to match
      * @return true if matches
      */
-    boolean matches(ExecutionContext executionContext, String value);
+    default boolean matches(ExecutionContext executionContext, String value) {
+        return true;
+    }
 
-    String getErrorMessageDescription();
+    /**
+     * Determines if the value matches the format.
+     * 
+     * @param executionContext  the execution context
+     * @param validationContext the validation context
+     * @param value             to match
+     * @return true if matches
+     */
+    default boolean matches(ExecutionContext executionContext, ValidationContext validationContext, String value) {
+        return matches(executionContext, value);
+    }
+
+    /**
+     * Determines if the value matches the format.
+     * <p>
+     * This can be implemented for non-string node types.
+     *
+     * @param executionContext the execution context
+     * @param validationContext the validation context
+     * @param node the node
+     * @param rootNode the root node
+     * @param instanceLocation the instance location
+     * @param assertionsEnabled if assertions are enabled
+     * @param formatValidator the format validator
+     * @return true if matches
+     */
+    default boolean matches(ExecutionContext executionContext, ValidationContext validationContext, JsonNode node,
+            JsonNode rootNode, JsonNodePath instanceLocation, boolean assertionsEnabled, FormatValidator formatValidator) {
+        JsonType nodeType = TypeFactory.getValueNodeType(node, validationContext.getConfig());
+        if (nodeType != JsonType.STRING) {
+            return true;
+        }
+        return matches(executionContext, validationContext, node.asText());
+    }
+
+    /**
+     * Validates the format.
+     * <p>
+     * This is the most flexible method to implement.
+     *
+     * @param executionContext the execution context
+     * @param validationContext the validation context
+     * @param node the node
+     * @param rootNode the root node
+     * @param instanceLocation the instance locaiton
+     * @param assertionsEnabled if assertions are enabled
+     * @param formatValidator the format validator
+     * @return the messages
+     */
+    default Set<ValidationMessage> validate(ExecutionContext executionContext, ValidationContext validationContext,
+            JsonNode node, JsonNode rootNode, JsonNodePath instanceLocation, boolean assertionsEnabled,
+            FormatValidator formatValidator) {
+        if (!matches(executionContext, validationContext, node, rootNode, instanceLocation, assertionsEnabled,
+                formatValidator)) {
+            if (assertionsEnabled) {
+                return Collections.singleton(formatValidator.message().instanceNode(node)
+                        .instanceLocation(instanceLocation).locale(executionContext.getExecutionConfig().getLocale())
+                        .failFast(executionContext.isFailFast())
+                        .arguments(this.getName(), this.getErrorMessageDescription()).build());
+            }
+        }
+        return Collections.emptySet();
+    }
 }
