e78395da1f270103538e334c17768fa8c04ce644
Meer Patel <meerpatel.1616@gmail.com>
2024-04-04 22:47:01 -0300

    added licence and default-not-found test case

--- src/test/java/com/networknt/schema/utils/DefaultsTest.java
+++ src/test/java/com/networknt/schema/utils/DefaultsTest.java
@@ -1,18 +1,12 @@
 package com.networknt.schema.utils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-
-import java.io.IOException;
-
 import org.junit.jupiter.api.Test;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.schema.JsonSchema;
 import com.networknt.schema.JsonSchemaFactory;
-import com.networknt.schema.JsonSchemaRef;
 import com.networknt.schema.SpecVersion.VersionFlag;
 public class DefaultsTest {
     
@@ -27,12 +21,14 @@ public class DefaultsTest {
     }
 
     @Test
-    void testGetDefaultNodeEquals() throws Exception {
+    void testGetDefaultNodeWhenDefaultNotFound() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = mapper.readTree("{\"default\": \"defaultValue\"}");
+        // Create a JsonNode without a "default" field
+        JsonNode node = mapper.readTree("{\"notDefault\": \"value\"}");
         JsonSchemaFactory factory = JsonSchemaFactory.getInstance(VersionFlag.V7);
         JsonSchema schema = factory.getSchema(node);
         JsonNode result = Defaults.getDefaultNode(schema);
-        assertEquals("defaultValue", result.asText(), "Default node should have the default value");
+        // Assert that the result is null, as there's no "default" node in the schema
+        assertNull(result, "Default node should be null when 'default' node is not found in the schema");
     }
 }
