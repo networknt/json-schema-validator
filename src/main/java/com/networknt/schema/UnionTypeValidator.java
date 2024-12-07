e596c08a8bf73ae2843c83160c97010f07aaffda
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2024-12-07 10:38:40 +0800

    Fix custom error message for union type

--- src/main/java/com/networknt/schema/UnionTypeValidator.java
+++ src/main/java/com/networknt/schema/UnionTypeValidator.java
@@ -35,7 +35,7 @@ public class UnionTypeValidator extends BaseJsonValidator implements JsonValidat
     private final String error;
 
     public UnionTypeValidator(SchemaLocation schemaLocation, JsonNodePath evaluationPath, JsonNode schemaNode, JsonSchema parentSchema, ValidationContext validationContext) {
-        super(schemaLocation, evaluationPath, schemaNode, parentSchema, ValidatorTypeCode.UNION_TYPE, validationContext);
+        super(schemaLocation, evaluationPath, schemaNode, parentSchema, ValidatorTypeCode.TYPE, validationContext);
         StringBuilder errorBuilder = new StringBuilder();
 
         String sep = "";
