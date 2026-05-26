88e211db1b362a3aaf17b9799f48edf6ee924edc
Steve Hu <stevehu@gmail.com>
2026-05-26 16:23:15 -0400

    fixes #1246 Coercion bug

--- src/main/java/com/networknt/schema/dialect/Dialect.java
+++ src/main/java/com/networknt/schema/dialect/Dialect.java
@@ -416,7 +416,7 @@ public class Dialect {
 
     private static String readText(JsonNode node, String field) {
         JsonNode fieldNode = node.get(field);
-        return fieldNode == null ? null : fieldNode.asString();
+        return fieldNode != null && fieldNode.isString() ? fieldNode.asString() : null;
     }
 
     public String getId() {
